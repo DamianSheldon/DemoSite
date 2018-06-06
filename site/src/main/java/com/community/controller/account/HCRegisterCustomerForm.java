3b84b8191257896fe329a4da76dc0ea35860ecdf
Meiliang Dong <dongmeilianghy@sina.com>
2018-06-06 17:31:30 +0800

    Finised Customize UI For Heat Clinic Tutorial.

--- site/src/main/java/com/community/controller/account/HCRegisterCustomerForm.java
+++ site/src/main/java/com/community/controller/account/HCRegisterCustomerForm.java
@@ -3,6 +3,9 @@ package com.community.controller.account;
 import org.broadleafcommerce.profile.web.core.form.RegisterCustomerForm;
 
 public class HCRegisterCustomerForm extends RegisterCustomerForm {
+
+	private static final long serialVersionUID = 1103219518136287615L;
+	
 	public String referralCode;
 
     public String getReferralCode() {
